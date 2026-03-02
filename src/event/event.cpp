e00d5d8e12c6f106cc940290ea2500e41f2d39aa
YiAiYuTian <2038991907@qq.com>
2026-03-02 20:50:10 +0800

    事件回调

--- src/event/event.cpp
+++ src/event/event.cpp
@@ -8,6 +8,11 @@ namespace lite
 struct Event::Impl
 {
     SDL_Event event;
+
+    void* data = nullptr;
+
+    QuitEventCallback quit_event_callback = nullptr;
+    KeyEventCallback key_event_callback = nullptr;
 };
 
 Event::Event()
@@ -22,17 +27,58 @@ Event::~Event()
 
 void Event::onUpdate()
 {
-    while (SDL_PollEvent(&m_impl->event))
+    auto& sdl_event = m_impl->event;
+    while (SDL_PollEvent(&sdl_event))
     {
-        switch (m_impl->event.type)
+        switch (sdl_event.type)
         {
         case SDL_EVENT_QUIT:
-            is_running = false;
+            if(m_impl->quit_event_callback) 
+                m_impl->quit_event_callback(m_impl->data);
+            break;
+        case SDL_EVENT_KEY_DOWN:
+            if(m_impl->key_event_callback)
+            {
+                m_impl->key_event_callback(
+                    sdl_event.key.key,
+                    sdl_event.key.repeat,
+                    sdl_event.key.down,
+                    sdl_event.key.mod,
+                    m_impl->data
+                );
+            }
+            break;
+        case SDL_EVENT_KEY_UP:
+            if(m_impl->key_event_callback)
+            {
+                m_impl->key_event_callback(
+                    sdl_event.key.key,
+                    sdl_event.key.repeat,
+                    sdl_event.key.down,
+                    sdl_event.key.mod,
+                    m_impl->data
+                );
+            }
             break;
         default:
             break;
         }
     }
+}
+
+void Event::setGlobalData(void *data)
+{
+    m_impl->data = data;
+}
+
+void Event::registerQuitEventCallback(QuitEventCallback callback)
+{
+    m_impl->quit_event_callback = callback;
+}
+
+void Event::registerKeyEventCallback(KeyEventCallback callback)
+{
+    m_impl->key_event_callback = callback;
 }
 
 }
--- src/event/event.h
+++ src/event/event.h
@@ -2,9 +2,18 @@
 #define EVENT_H
 
 #include "../core/core.h"
+#include "keycode.h"
+
+#include <stdint.h>
 
 namespace lite
 {
+
+typedef uint32_t Keycode;
+typedef uint16_t Keymod;
+
+typedef void (*QuitEventCallback)(void* data);
+typedef void (*KeyEventCallback)(Keycode keycode, bool repeat, bool down, Keymod keymod, void* data);
 
 class LITE_API Event
 {
@@ -13,7 +22,10 @@ public:
     ~Event();
 
     void onUpdate();
-    bool is_running = true;
+
+    void setGlobalData(void* data);
+    void registerQuitEventCallback(QuitEventCallback callback);
+    void registerKeyEventCallback(KeyEventCallback callback);
 private:
     struct Impl;
     Impl* m_impl;
