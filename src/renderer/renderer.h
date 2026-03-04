47d62a189220489c50fe89bc62b2f4587c921bdb
YiAiYuTian <2038991907@qq.com>
2026-03-04 21:24:37 +0800

    渲染函数

--- src/renderer/renderer.h
+++ src/renderer/renderer.h
@@ -2,6 +2,7 @@
 #define RENDERER_H
 
 #include "../core/core.h"
+#include "../math/math.h"
 
 namespace lite
 {
@@ -18,6 +19,26 @@ public:
     Renderer& operator=(const Renderer&) = delete;
     Renderer(Renderer&&) = delete;
     Renderer& operator=(Renderer&&) = delete;
+
+    void beginDraw(Color background_color = { 0, 0, 0, 255 });
+    void endDraw();
+
+    //tools
+    void drawPoint(const Vector2f& pos, Color color = { 255, 255, 255, 255 });
+    void drawPoints(const Vector2f* pos, int count, Color color = { 255, 255, 255, 255 });
+    void drawLine(const Vector2f& start, const Vector2f& end, Color color = { 255, 255, 255, 255 });
+    void drawRect(const Vector2f& pos, const Vector2f& size, Color color = { 255, 255, 255, 255 });
+    void drawRect(const FRect& rect, Color color = { 255, 255, 255, 255 });
+    void drawFillRect(const Vector2f& pos, const Vector2f& size, Color color = { 255, 255, 255, 255 });
+    void drawFillRect(const FRect& rect, Color color = { 255, 255, 255, 255 });
+    
+    void fdrawPoint(const Vector2f& pos, FColor color = { 1.0f, 1.0f, 1.0f, 1.0f });
+    void fdrawPoints(const Vector2f* pos, int count, FColor color = { 1.0f, 1.0f, 1.0f, 1.0f });
+    void fdrawLine(const Vector2f& start, const Vector2f& end, FColor color = { 1.0f, 1.0f, 1.0f, 1.0f });
+    void fdrawRect(const Vector2f& pos, const Vector2f& size, FColor color = { 1.0f, 1.0f, 1.0f, 1.0f });
+    void fdrawRect(const FRect& rect, FColor color = { 1.0f, 1.0f, 1.0f, 1.0f });
+    void fdrawFillRect(const Vector2f& pos, const Vector2f& size, FColor color = { 1.0f, 1.0f, 1.0f, 1.0f });
+    void fdrawFillRect(const FRect& rect, FColor color = { 1.0f, 1.0f, 1.0f, 1.0f });
 private:
     struct Impl;
     Impl* m_impl = nullptr;
